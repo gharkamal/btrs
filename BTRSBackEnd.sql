93835f0eccd69446953fc480fbdf85157490af73
Yimin Mei <yiminmei@Yimins-MacBook-Pro.local>
2017-11-14 14:05:28 -0800

    -initial settings

---
+++ BTRSBackEnd.sql
@@ -0,0 +1,61 @@
+DROP DATABASE IF EXISTS BTRS; 
+CREATE DATABASE BTRS;
+USE BTRS;
+
+DROP TABLE IF EXISTS Passenger;
+CREATE TABLE Passenger(
+passengerID INT,
+accountID INT, 
+startStID INT,
+endStID INT,
+seatID INT, 
+dateTime date,
+wifi boolean,
+PRIMARY KEY(passengerID)
+);
+DROP TABLE IF EXISTS Account_Holder; 
+CREATE TABLE Account_Holder(
+accountID INT,
+fullName VARCHAR(50), 
+email VARCHAR(30),
+password VARCHAR(30), 
+creditCard INT,
+PRIMARY KEY(accountID)
+);
+
+
+DROP TABLE IF EXISTS Train;
+CREATE TABLE Train(
+trainID INT PRIMARY KEY,
+deptTime TIME,
+isFull BOOL
+);
+
+
+DROP TABLE IF EXISTS Station;
+CREATE TABLE Station(
+stationID INT PRIMARY KEY,
+name VARCHAR(64),
+orderNumber INT
+);
+
+
+DROP TABLE IF EXISTS Car;
+CREATE TABLE Car(
+carNumber INT,
+seatID VARCHAR(3),
+trainID INT,
+passengerID INT DEFAULT NULL,
+PRIMARY KEY(carNumber, seatID, trainID),
+FOREIGN KEY(trainID) REFERENCES Train(trainID) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY(passengerID) REFERENCES Passenger(passengerID) ON UPDATE CASCADE ON DELETE CASCADE 
+);
+
+
+DROP TABLE IF EXISTS Banned; 
+CREATE TABLE Banned(
+accountID INT REFERENCES Account_Holder(accountID) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
